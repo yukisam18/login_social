2d18871c0abbd05abc9b0ed5d93485670759ce7f
Yuki Neves <yuki.neves@w3as.com>
2019-07-30 17:50:47 -0300

    Retorno do facebook

--- lib/login.dart
+++ lib/login.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:login_social/signup.page.dart';
-
-
-void loginFacebook(){
-
-}
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Login extends StatelessWidget {
+  void loginFacebook() async {
+    var login = FacebookLogin();
+    var result = await login.logInWithReadPermissions(['email']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.error:
+        print("Login Erro!");
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("Cancelado pelo usuário");
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print("Logado com sucesso!");
+        final token = result.accessToken.token;
+        final graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${token}');
+        final profile = json.decode(graphResponse.body);
+
+        print(profile['name']);
+        print(profile['last_name']);
+        print(profile['id']);
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +61,10 @@ class Login extends StatelessWidget {
               ),
             ),
           ),
-           SizedBox(
+          SizedBox(
             height: 10,
           ),
-           TextFormField(
+          TextFormField(
             keyboardType: TextInputType.text,
             obscureText: true,
             decoration: InputDecoration(
@@ -53,66 +76,47 @@ class Login extends StatelessWidget {
               ),
             ),
           ),
-
-           SizedBox(
-              height: 40,
-            ),
-             Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                color: Color(0xFF3C5A99),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Login com Facebook",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 15,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      Container(
-                        child: SizedBox(
-                          child: Image.asset("imagens/fb-icon.png"),
-                          height: 28,
-                          width: 28,
-                        ),
-                      )
-                    ],
-                  ),
-                  onPressed: () {},
-                ),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            height: 60,
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              color: Color(0xFF3C5A99),
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 40,
+            child: SizedBox.expand(
               child: FlatButton(
-                child: Text(
-                  "Cadastre-se",
-                  textAlign: TextAlign.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Login com Facebook",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    Container(
+                      child: SizedBox(
+                        child: Image.asset("imagens/fb-icon.png"),
+                        height: 28,
+                        width: 28,
+                      ),
+                    )
+                  ],
                 ),
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SignupPage(),
-                    ),
-                  );
+                  loginFacebook();
                 },
               ),
             ),
+          ),
         ],
       ),
     ));
